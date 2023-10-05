8c8bbac97f9d389cb7ea671355eeb89de90efc52
Aryan22codder <103300045+Aryan22codder@users.noreply.github.com>
2023-10-05 23:21:52 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+class Variables
+{
+    public static void main(String args[])
+    {
+       byte b = 127;
+       short s = 32760;
+       int i = 100;
+
+       float f = 25.4f;
+       char c ='A';
+       System.out.println("b");
+       System.out.println("s");
+       System.out.println("i");
+       System.out.println("f");
+       System.out.println("c");
+
+
+    }
+}
